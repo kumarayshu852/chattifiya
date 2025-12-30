b839a31273266851bcc827df8f238abb9e4ace06
Gandhi Tech AI <kumarayshu852@gmail.com>
2025-12-30 14:28:05 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser())
 app.use(cors({
-    origin: ture,
+    origin: true,
     credentials: true,
 }));
 
