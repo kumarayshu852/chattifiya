f1d8b7f1e365e315261c095fd0c6e8c941af46bc
Gandhi Tech AI <kumarayshu852@gmail.com>
2025-12-30 13:18:30 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,7 @@ if(process.env.NODE_ENV=== "production"){
     app.use(express.static(path.join(_dirname,"../frontend/dist")));
 
 
-    app.get("*",(req,res)=>{
+    app.get("/*",(req,res)=>{
         res.sendFile(path.join(_dirname,"../frontend","dist", "index.html"))
     })
 }
